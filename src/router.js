f61bf58de98eeea5261113152a452e163ce9f6e2
Антон Иванов <aivanov@kmis.ru>
2019-05-25 14:50:46 +0300

    Страница с выводом анкет

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Surveys from "./views/Surveys.vue";
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/surveys",
+      name: "Surveys",
+      component: Surveys
     },
     {
       path: "/about",
---
+++ src/views/Surveys.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="surveys">
+    <div>sdvsdvs</div>
+    <div>
+      Поиск анкеты
+      <div>
+        <form>
+          <input type="text">№ кандидата
+          <input type="text">
+ФИО кандидата
+        </form>
+      </div>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>№ кандидата</th>
+          <th>ФИО</th>
+          <th>Статус</th>
+          <th>Дата создания</th>
+          <th>Дата редактирования</th>
+        </tr>
+        <tr v-for="survey in surveys" :key="survey.id">
+          <td>{{survey.id}}</td>
+          <td>{{survey.name}}</td>
+          <td>{{survey.status}}</td>
+          <td>{{survey.created_date}}</td>
+          <td>{{survey.modyfied_date}}</td>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "surveys",
+  data: () => ({
+    surveys: [
+      {
+        id: "1",
+        name: "Васька",
+        status: "Уточняется",
+        created_date: "2011-9-31",
+        modyfied_date: "2011-9-31"
+      },
+      {
+        id: "2",
+        name: "Мурка",
+        status: "Заполнена",
+        created_date: "2011-9-31",
+        modyfied_date: "2011-9-31"
+      },
+      {
+        id: "3",
+        name: "Васька",
+        status: "Создана",
+        created_date: "2011-9-31",
+        modyfied_date: "2011-9-31"
+      }
+    ]
+  }),
+  components: {}
+};
+</script>
