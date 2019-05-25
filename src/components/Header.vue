b229c6d6b14048936fe841007fcbacfb5682f763
Сергей <sergey@MacBook-Air-Sergej.local>
2019-05-25 19:08:27 +0300

    Хеадер и json-поля

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">Карельский регистр неродственных доноров гемопоэтических стволовых клеток</div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  color: white;
+  background-color: darkblue;
+  padding: 10px 20px;
+}
+</style>
